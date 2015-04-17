5892e9cac5534ae22d9574e5be2367b32313adf4
Alexis Algoud <alexis@atm-consulting.fr>
2015-04-17 12:21:09 +0200

    NEW popin for tab

--- js/speednav.js.php
+++ js/speednav.js.php
@@ -3,47 +3,106 @@
     require('../config.php');
     
 ?>
+
+var TNoLink=[
+    "admin/modules.php"
+];
+
 $(document).ready(function() {
    
-   initLink();
-   
+   initLink("div.tmenudiv ul.tmenu a, div#id-left div.vmenu a");
+   initPopin("div.fiche div.tabs div.tabsElem a:not(:first)");
     
 });
-
-function initLink(onWhat) {
+function initPopin(onWhat) {
+    
    
-   if(onWhat==null){ onWhat="div.tmenudiv ul.tmenu a, div#id-left div.vmenu a";}
+    $(onWhat).each(function(i) {
+       
+         var url = $(this).attr("href"); 
+         $(this).attr("href","javascript:;");
+         $(this).attr("ajax-url", url);
+         
+        $(this).click(function(event) {
+                var url = $(this).attr("ajax-url");
+                var title = $(this).text();
+                
+                $("#popin-full-screen-tab").remove();
+                $("body").append('<div id="popin-full-screen-tab"></div>' );
+                
+                $( "div#popin-full-screen-tab" ).load( url+ " div#id-container div.fiche", function( response, status, xhr ) {
+                    
+                    if(response=="") {
+                        document.location.href = url;
+                    }
+                    else {
+                       $( "div#popin-full-screen-tab div.tabs" ).remove();
+                       $( "div#popin-full-screen-tab" ).dialog({
+                          dialogClass: "fulldialog"
+                          ,modal:false
+                          ,width:"80%"
+                          ,title:title
+                          
+                        });
+                       
+                    }
+             
+                });
+                
+           });    
+            
+    });
+}
+function initLink(onWhat) {
    
    $(onWhat).each(function(i) {
        
        var url = $(this).attr("href");
-       $(this).attr("href","javascript:;");
-       $(this).attr("ajax-url", url);
-        
-       $(this).click(function(event) {
-            var url = $(this).attr("ajax-url");
+       
+       if(!isForbiddenLink(url)) {
+               
+           $(this).attr("href","javascript:;");
+           $(this).attr("ajax-url", url);
             
-            if($("div#id-container-master").length==0) {
-                $("div#id-container").wrap( "<div id=\"id-container-master\" style=\"width:100%;\"></div>" );
-            }
-            
-         //   $( "div#id-container-master" ).css({ opacity:0.2 });
-            $( "div#id-container-master" ).load( url+ " div#id-container", function( response, status, xhr ) {
+           $(this).click(function(event) {
+                var url = $(this).attr("ajax-url");
                 
-                if(response=="") {
-                    document.location.href = url;
+                if($("div#id-container-master").length==0) {
+                    $("div#id-container").wrap( "<div id=\"id-container-master\" style=\"width:100%;\"></div>" );
                 }
-                else {
-            //       $( "div#id-container-master" ).css({ opacity:1 });
-                   initLink("div#id-left div.vmenu a");
-                }
-         
-            });
-            
-       });
+                
+             //   $( "div#id-container-master" ).css({ opacity:0.2 });
+                $( "div#id-container-master" ).load( url+ " div#id-container", function( response, status, xhr ) {
+                    
+                    if(response=="") {
+                        document.location.href = url;
+                    }
+                    else {
+                //       $( "div#id-container-master" ).css({ opacity:1 });
+                       initLink("div#id-left div.vmenu a");
+                    }
+             
+                });
+                
+           });          
+       } 
+       
+
    
    });
      
     
     
+}
+
+function isForbiddenLink(url) {
+    
+    for(i in TNoLink) {
+        if(url.indexOf(TNoLink[i])>-1 ) {
+            console.log(url, TNoLink[i]);
+            return true;
+            
+         }
+    }
+    return false;
 }
